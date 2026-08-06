0d6e2ead53397ec5579a8685e7e72662a9a5edf7
yzha0719 <yzha0719@student.monash.edu>
2026-08-06 18:19:53 +1000

    Change the comment.

--- yzhang-library/src/components/JSON.vue
+++ yzhang-library/src/components/JSON.vue
@@ -95,7 +95,7 @@
     </section>
 
     <section class="lab-section">
-      <!-- [OPTIONAL - NON ASSESSED] Activity 6: Attribute, Class and Style Bindings -->
+      <!-- Activity 5: Attribute, Class and Style Bindings -->
       <h2>Attribute, Class and Style Binding with <code>v-bind</code></h2>
       <p>Highlighting Specific Authors:</p>
       <ul>
