7b967838cbc4ee4e88ea4be0e5d0f159997dec23
yzha0719 <yzha0719@student.monash.edu>
2026-08-06 17:11:27 +1000

    Completed task1.2

--- fit5032-week1/src/App.vue
+++ fit5032-week1/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MyName from './components/MyName.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+    <MyName />
     <TheWelcome />
   </main>
 </template>
---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,14 @@
+<script setup>
+const name = 'Yiming Zhang'
+</script>
+
+<template>
+  <h2 class="my-name">{{ name }}</h2>
+</template>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  color: var(--color-heading);
+}
+</style>
